cb31522f0960df10072eb6e208c0dce2ec3fac83
Jerald Espares <145651483+JeraldSpares@users.noreply.github.com>
2026-05-09 04:38:37 +0800

    Add files via upload

---
+++ resources/views/employees/index.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts.app')
+@section('head')
+@include('includes.head',['title'=> trans('Employees')])
+@endsection
+@section('css')
+<style>
+    /* Minimal padding for overall structure */
+    .row.p-2, .col-mobile, .board-box, .table-style {
+        padding: 2px 5px;
+        margin: 0;
+    }
+
+    /* Make table compact */
+    .data-table th, .data-table td {
+        padding: 4px 6px;
+        font-size: 14px; /* Smaller font for compact look */
+        vertical-align: middle;
+    }
+
+    /* Compact buttons */
+    .btn-default {
+        padding: 2px 5px;
+        font-size: 12px;
+        margin-right: 3px; /* Space between buttons */
+    }
+
+    /* Arrange action buttons horizontally */
+    .custom-column {
+        white-space: nowrap;
+    }
+
+    .custom-column .btn-group {
+        display: inline-flex;
+    }
+
+    /* Google Calendar icon */
+    .google-calendar {
+        width: 20px;
+        height: 20px;
+        margin-top: 2px;
+    }
+
+    /* Employee Badge for Google Sync Status */
+    .employee-badge i {
+        font-size: 16px;
+        color: #28a745;
+    }
+</style>
+@endsection
+@section('content')
+<div class="row p-2">
+    @include('includes.message-block')
+    <div class="col-sm-12 col-mobile">
+        <div class="board-box">
+            <div class="board-title">
+                <h2>{{ __('List of all employees') }} <a href="{{ route('employees.create') }}" class="add-new-employee"><span
+                            class="fa fa-plus pull-c-right"></span></a></h2>
+                    
+            </div>
+    
+            <div class="table-style">
+                <div class="table-responsive">
+                    <table class="table table-hover data-table" id="emp-table">
+                        <thead>
+                            <tr>
+                                <th>{{ __('SR No.') }}</th>
+                                <th>{{ __('First Name') }}</th>
+                                <th>{{ __('Last Name') }}</th>
+                                <th>{{ __('Email') }}</th>
+                                <th>{{ __('Phone') }}</th>
+                                @if($custom->categories == 1)
+                                    <th>{{ucfirst($custom->custom_field_category)}}</th>
+                                @endif
+                                <th>{{ucfirst($custom->custom_field_service)}}</th>
+                                <th>{{ __('Status') }}</th>
+                                <th></th>
+                                <th class="custom-column">{{ __('Action') }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($employees as $employee)
+                            <tr>
+                                <th>{{ $rowIndex++ }}</th>
+                                <td>{{ ucfirst($employee->first_name) }}</td>
+                                <td>{{ ucfirst($employee->last_name) }}</td>
+                                <td>{{ $employee->email }}</td>
+                                <td>{{ $employee->country_code.$employee->phone }}</td>
+                                @if($custom->categories == 1)
+                                    <td>{{ ucfirst($employee->categories) }}</td>
+                                @endif
+                                <td>{{ $employee->services }}</td>
+                                <td>
+                                    <input type="checkbox" name="status" class="status" value="1" @if($employee->status == 1) {{ 'checked' }} @endif 
+                                    data-toggle="toggle" data-style="slow" data-onstyle="success" data-offstyle="danger" data-off={{ __('Inactive') }} 
+                                    data-employee_id="{{$employee->id}}" data-on="{{ __('Active') }}">
+                                </td>
+                                <td>
+                                    {{-- <a href="{{ Helper::googlecalendar($employee->email, $employee->id) }}"> --}}
+                                    <a @if($employee->google_verify == true) {{ 'disabled' }} href="javascript:void(0)" class="remove-google" data-id="{{$employee->id}}" @else onclick="return googleCalendarEmailConfirmation(this);" data-href="{{ route('SendEmailGoogleCalenderLink',$employee->id) }}" @endif>
+                                        @if(isset($employee->google_verify) && $employee->google_verify == true)  
+                                        {{ Form::open(['method' => 'DELETE','id' => 'removeItem','route' => ['removegoogle',$employee->id]]) }}
+                                            <span class="employee-badge"><i class="fa fa-check d-flex btn-disconnect"></i></span>
+                                        {{ Form::close() }}
+                                        @endif
+                                        <img alt="Connect With Google Calendar" title="Connect With Google Calendar" class="google-calendar mt-3" height="25" width="25" src="{{ asset('img/employee/calendar.png')}}">
+                                    </a>
+                                    {{-- <button type="submit" id="gcalendar" class="btn shadow-none mt-1" name="submit" value="">
+                                        <img alt="Google Calendar" title="Connect With Google Calendar" class="" height="30" width="30" src="{{ asset('img/employee/calendar.png')}}"/>
+                                    </button> --}}
+                                </td>
+                                <td>
+                                    <a class="btn btn-default btn-lg" href="{{ route('employees.show',$employee->id) }}">
+                                        <span class="glyphicon glyphicon-eye-open"></span>
+                                    </a>
+                                    <a class="btn btn-default btn-lg" href="{{ route('employees.edit',$employee->id) }}">
+                                        <span class="glyphicon glyphicon-edit"></span>
+                                    </a>
+                                    <a class="btn btn-default btn-lg" title="{{ __('appointment') }}" href="{{ route('employees.appointment',$employee->id) }}">
+                                        <span class="glyphicon glyphicon-calendar"></span>
+                                    </a> 
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+    
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+<script src="{{ asset('backend/js/employee.js')}}"></script>
+@endsection
+
---
+++ resources/views/front/include/home-footer.blade.php
@@ -0,0 +1,8 @@
+<div class="container footer-bottom clearfix">
+    <div class="copyright">
+        &copy; {{ __('Copyright') }} <strong><span>{{ __('Quantum Care Appointment System') }}</span></strong>. {{ __('All Rights Reserved') }}
+    </div>
+    <div class="credits">
+        {{ __('Designed by') }} <a href="{{route('welcome')}}">{{ __('Quantum Care') }}</a>
+    </div>
+</div>
